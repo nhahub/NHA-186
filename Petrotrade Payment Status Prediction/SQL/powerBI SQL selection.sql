9985b2f18b3960b2bf8b42545d19ede145b61d6d
ahmed0elshafey <ahmed0elshafey0@gmail.com>
2025-12-05 15:59:09 +0200

    1

--- Petrotrade Payment Status Prediction/SQL/powerBI SQL selection.sql
+++ Petrotrade Payment Status Prediction/SQL/powerBI SQL selection.sql
@@ -1,2 +1,27 @@
-SELECT "C_ID", contract_date, consumption, installment_most_faw, remaining_installments_faw, bank_installment, remaining_bank_installments, customer_type, total_financial_amount, invoice_payment_date, contract_age_days, invoice_age_days, prediction, label
+SELECT "C_ID",
+contract_date, 
+consumption, 
+installment_most_faw, 
+remaining_installments_faw, 
+bank_installment, 
+remaining_bank_installments, 
+customer_type, 
+total_financial_amount, 
+invoice_payment_date, 
+contract_age_days, 
+invoice_age_days, 
+prediction, label
 	FROM public."PT_payment_status"
+
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+
+.
